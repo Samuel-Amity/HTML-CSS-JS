331cdfdd787665cb1fe7a7bff2f2cf2a02eaa560
Samuel Nathan <144701270+Samuel-Amity@users.noreply.github.com>
2024-01-24 15:40:04 +0530

    Day 12.1

--- Javascript/Class 3/index.js
+++ Javascript/Class 3/index.js
@@ -8,6 +8,8 @@
 // }
 // while(x<=30) 
 
+
+
 // ---------- FOR LOOP
 
 // for(var x=1; x<=10;x++)
@@ -17,11 +19,34 @@
 
 // Continue and break
 
-for (var x = 5; x <= 50; x += 5) {
-   // if (x == 5){break;}
-    document.write( x + "<br>");
-}
+// for (var x = 5; x <= 50; x += 5) {
+//    // if (x == 5){break;}
+//     document.write( x + "<br>");
+// }
 
 // While loop ka use karke aapko 100-10 tak ka diff print karne hai - diffrence rahega 2 ka opposite me (100,98,96) 
+// var x = 1;
+
+
+// var x = 100;
+
+// while (x >= 10) {
+//     document.write(x + '<br>');
+//     x = x - 2;
+// }
+
 // do while ka use karke 80-10 tak ka diffrence print karna hai - 4 ka
+
+// var x = 80;
+// do {
+//     document.write(x + "<br>"); // ---------
+//     x -= 4;
+// } while(x >= 10);
+
 // for loop ka use karke 10-100 ka no print karne hai - diffrence 5 ka
+
+
+
+for (var x = 100; x >= 10; x -= 5) {
+    document.write(x + "<br>");
+}
